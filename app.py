d4755b1ea9c521da95e322a967a7498b47b5fee3
Karla Espinoza <kespinoza.rm@gmail.com>
2024-11-16 14:40:09 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Hello World!")
+import streamlit as st
+import pandas as pd
+
+# 1. Text inputs
+
+favorite_color = st.text_input("What's your favorite color:")
+st.write(f"My favorite color is {favorite_color}")
+
+favorite_animal= st.text_input("What's your favorite animal:")
+st.write(f"My favorite animal is {favorite_animal}")
+
+lucky_number= st.number_input("What's your lucky number:", step = 1)
+st.write(f"My lucky number is {lucky_number}")
+
+superpower = st.selectbox("Choose your superpower:", ["Flying", "Invisibiblity", "Fast as flash"])
+
+
+
+if st.button("Generate my superhero name 🦹🏻") == True:
+    st.header(f"Your superhero name is: {favorite_animal} of {favorite_color} {lucky_number}")
+    st.header(f"And your superpower is: {superpower}")
